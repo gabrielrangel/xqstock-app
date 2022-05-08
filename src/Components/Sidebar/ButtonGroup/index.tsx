5c8377341fb793bc8c9c58a2575128eca1e86c07
Gabriel Rangel <grangelda@icloud.com.br>
2022-05-07 23:45:00 -0300

    feat( 6-correção-n…o-na-sidebar) adicionado wrapper para os botoes

---
+++ src/Components/Sidebar/ButtonGroup/index.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+import { BoxProps } from "@mui/material/Box/Box";
+import Box from "./styles";
+
+export const SidebarButtonGroup: FunctionComponent<BoxProps> = (props) => (
+  <Box {...props} />
+);
+
+export default SidebarButtonGroup;
