c4979cbd5313941ebf34d561bd5b6788794ee2c1
Gabriel Rangel <grangelda@icloud.com.br>
2022-05-11 19:21:14 -0300

    feat(11-criar-layout-do-dashboard wip) adicionado input para os filtros wip

--- src/Components/Dashboard/Filter/MetadataAutoComplete/index.tsx
+++ src/Components/Dashboard/Filter/MetadataAutoComplete/index.tsx
@@ -4,59 +4,49 @@ import {
   FunctionComponent,
   SyntheticEvent,
   useCallback,
-  useEffect,
+  useMemo,
   useState,
 } from "react";
 
-import {
-  SymbolMetadataActionKind,
-  SymbolMetadataState,
-} from "../../../../Reducers/SymbolMetadataReducer";
+import { SymbolMetadataState } from "../../../../Reducers/SymbolMetadataReducer";
 
 import { ISymbolMetadata } from "../../../../Services/XqStockApi/types";
 import MetadataInputOption from "./Option";
 import MetadataAutoCompleteProps from "./types";
 import MetadataAutoCompleteInput from "./Input";
+import onMetadataAutoCompleteChange from "./onChange";
 
 export const MetadataAutoComplete: FunctionComponent<
   MetadataAutoCompleteProps
 > = ({ placeholder, fetchOptions, valueReducer }) => {
   const [options, setOptions] = useState<SymbolMetadataState>([]);
   const [inputValue, setInputValue] = useState<string>("");
-  const [value, dispatchValue] = valueReducer;
+  const [value] = valueReducer;
 
-  useEffect(() => {
+  useMemo(() => {
     if (inputValue) {
       fetchOptions(inputValue).then((result) => setOptions(result));
     }
   }, [fetchOptions, inputValue]);
 
   const onChange = useCallback(
-    (event: SyntheticEvent<Element, Event>, newValue: ISymbolMetadata[]) => {
-      newValue
-        ?.filter(
-          ({ Symbol }) =>
-            !value.some((v: ISymbolMetadata) => v.Symbol === Symbol)
-        )
-        .forEach((payload) =>
-          dispatchValue({ type: SymbolMetadataActionKind.ADD, payload })
-        );
-    },
-    [dispatchValue, value]
+    (event: SyntheticEvent<Element, Event>, newValue: ISymbolMetadata[]) =>
+      onMetadataAutoCompleteChange(event, newValue, valueReducer),
+    [valueReducer]
   );
 
   return (
     <Autocomplete
-      disablePortal
-      multiple
-      value={value}
       options={options}
       renderInput={(params) => (
         <MetadataAutoCompleteInput params={params} placeholder={placeholder} />
       )}
+      disablePortal
       getOptionLabel={({ Symbol, Name }) => `${Symbol} ${Name}`}
+      multiple
       onChange={onChange}
       onInputChange={(event, newInputValue) => setInputValue(newInputValue)}
+      value={value}
       renderOption={MetadataInputOption}
     />
   );
