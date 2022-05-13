795398e22140121bd2fe48f1155412f6ba91dc47
Gabriel Rangel <grangelda@icloud.com.br>
2022-05-13 14:24:41 -0300

    feat(11-criar-layout-do-dashboard wip) adicionado input para os filtros wip

--- src/Components/Dashboard/FilterGroup/index.tsx
+++ src/Components/Dashboard/FilterGroup/index.tsx
@@ -2,15 +2,10 @@ import Container from "@mui/material/Container";
 import { FunctionComponent, ReactNode } from "react";
 import StyledBox from "./styles";
 import Grid from "@mui/material/Grid";
-import useDashboardContext from "../../../Hooks/useDashboardContext";
 
 export const DashboardFilterGroup: FunctionComponent<{
   children: ReactNode;
 }> = ({ children }) => {
-  const { stockMetadataReducer, timeIntervalState } = useDashboardContext();
-  const [stockMetadata] = stockMetadataReducer;
-  const [timeInterval] = timeIntervalState;
-
   return (
     <StyledBox>
       <Container maxWidth="lg">
